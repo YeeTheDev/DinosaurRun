48f606678f960bd8272941f5e4878a1203d498e2
Yee <yee.devindie@gmail.com>
2024-09-17 13:26:44 -0600

    Speed is handled globally

---
+++ Assets/Scripts/SpeedHandler.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace DinoRun.Core
+{
+    public class SpeedHandler : MonoBehaviour
+    {
+        public static float globalSpeed;
+
+        [SerializeField] float startingSpeed = 0.2f;
+        [SerializeField] float speedPerSecond;
+        [SerializeField] float maxSpeed = 10f;
+
+        float timer = 1;
+
+        private void Awake() => globalSpeed = startingSpeed;
+
+        private void Update()
+        {
+            if (globalSpeed < maxSpeed && Time.timeSinceLevelLoad >= timer)
+            {
+                timer = Time.timeSinceLevelLoad + 1;
+                globalSpeed = Mathf.Clamp(globalSpeed + speedPerSecond, startingSpeed, maxSpeed);
+            }
+        }
+    }
+}
